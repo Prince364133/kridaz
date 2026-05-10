712de94b91166c78e47e9a69f042a02078b0cc64
Prince364133 <monsterprince3633@gmail.com>
2026-05-10 14:09:53 +0530

    fix: corrected routing for my joined games in navbar

--- client/user/src/user/components/layout/Navbar.jsx
+++ client/user/src/user/components/layout/Navbar.jsx
@@ -255,7 +255,7 @@ const Navbar = () => {
                         </Link>
 
                         <Link 
-                          to="/my-joined-matches" 
+                          to="/my-joined-games" 
                           className="flex items-center gap-3 p-3 rounded-xl hover:bg-white/5 text-white/70 hover:text-white transition-all"
                         >
                           <Trophy size={18} className="text-white/40" />
