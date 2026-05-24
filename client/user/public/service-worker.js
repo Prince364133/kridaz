e468b0bc208ca22b2e205456d1e84249adf52aaa
Prince364133 <monsterprince3633@gmail.com>
2026-05-25 04:04:22 +0530

    fix: replace deleted service worker with a self-destructing tombstone to force immediate client-side cache clearing

---
+++ client/user/public/service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => {
+      self.registration.unregister();
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
